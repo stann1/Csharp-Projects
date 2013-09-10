9a19197d8acd5343d7a8b44d955b41448aceeb95
stann1 <stoychev.st2@gmail.com>
2013-09-10 12:01:54 +0300

    Added xml reader task

--- ASP.NET WebForms/HW05 - DataBinding/07.XmlReader/XmlReaderForm.aspx.designer.cs
+++ ASP.NET WebForms/HW05 - DataBinding/07.XmlReader/XmlReaderForm.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace _07.XmlReader {
         protected global::System.Web.UI.HtmlControls.HtmlForm FormMain;
         
         /// <summary>
+        /// XmlDataSourceBooks control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSourceBooks;
+        
+        /// <summary>
         /// TreeViewXmlData control.
         /// </summary>
         /// <remarks>
